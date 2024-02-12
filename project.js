5b336bee509dc282c32fca88bda6db5df09543b4
FakeGods <w.leszkowicz99@gmail.com>
2024-02-12 21:13:39 +0100

    lines

--- project.js
+++ project.js
@@ -1,11 +1,31 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-  const depositAmount = prompt("Enter a deposit amount: ");
-  const numberDepositAmount = parseFloat(depositAmount);
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
 
-  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter number of lines to bet on (1-3)");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid lines number, try again.");
+    } else {
+      return numberOfLines;
+    }
   }
 };
 
-deposit();
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
+
+console.log(numberOfLines);
